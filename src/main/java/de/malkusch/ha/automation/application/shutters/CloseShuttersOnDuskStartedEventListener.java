192e3aa48a4b5409a7bfb7f502e5fddd34660749
Markus Malkusch <markus@malkusch.de>
2022-06-18 00:01:49 +0200

    Don't close shutters in a hot summer night

--- src/main/java/de/malkusch/ha/automation/application/shutters/CloseShuttersOnDuskStartedEventListener.java
+++ src/main/java/de/malkusch/ha/automation/application/shutters/CloseShuttersOnDuskStartedEventListener.java
@@ -1,56 +1,62 @@
 package de.malkusch.ha.automation.application.shutters;
 
 import static de.malkusch.ha.automation.model.shutters.ShutterId.TERRASSE;
+import static java.util.Arrays.asList;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import de.malkusch.ha.automation.model.Temperature;
 import de.malkusch.ha.automation.model.astronomy.AstronomicalEvent.AstronomicalSunsetStarted;
 import de.malkusch.ha.automation.model.astronomy.AstronomicalEvent.CivilSunsetStarted;
+import de.malkusch.ha.automation.model.shutters.Shutter;
 import de.malkusch.ha.automation.model.shutters.ShutterRepository;
 import de.malkusch.ha.automation.model.weather.Weather;
 import de.malkusch.ha.shared.model.ApiException;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RequiredArgsConstructor
 @Service
 @Slf4j
 public class CloseShuttersOnDuskStartedEventListener {
 
-    @ConfigurationProperties("shutters.blinds.terrasse")
-    @Component
-    @Data
-    public static class Properties {
-        private double nightCloseThreshold;
-    }
-
     private final ShutterRepository shutters;
-    private final Properties properties;
     private final Weather weather;
+    private final Temperature coldNightTemperature;
+
+    public CloseShuttersOnDuskStartedEventListener(
+            @Value("${shutters.cold-night-temperature}") double coldNightTemperature, ShutterRepository shutters,
+            Weather weather) {
+
+        this.shutters = shutters;
+        this.weather = weather;
+        this.coldNightTemperature = new Temperature(coldNightTemperature);
+    }
 
     @EventListener
     public void onDuskStarted(CivilSunsetStarted event) throws ApiException, InterruptedException {
-        log.info("Closing shutters at dusk");
-        for (var shutter : shutters.findAll()) {
-            if (shutter.id == TERRASSE) {
-                continue;
-            }
-            shutter.close();
-        }
+        var allButTerrasse = shutters.findAll().stream().filter(it -> it.id != TERRASSE).toList();
+        closeShuttersWhenColdNight(allButTerrasse);
     }
 
     @EventListener
     public void onNightStarted(AstronomicalSunsetStarted event) throws ApiException, InterruptedException {
-        var threshold = new Temperature(properties.nightCloseThreshold);
-        if (weather.temperature().isLessThan(threshold)) {
-            log.info("Closing Terrasse blinds during a cold night");
-            var blind = shutters.find(TERRASSE);
-            blind.close();
+        var onlyTerrasse = asList(shutters.find(TERRASSE));
+        closeShuttersWhenColdNight(onlyTerrasse);
+    }
+
+    private void closeShuttersWhenColdNight(Collection<Shutter> shutters) throws ApiException, InterruptedException {
+        var temperature = weather.temperature();
+        if (!temperature.isLessThan(coldNightTemperature)) {
+            log.info("Night too warm ({}) for closing shutters", temperature);
+            return;
+        }
+
+        log.info("Closing shutters during a cold night ({})", temperature);
+        for (var shutter : shutters) {
+            shutter.close();
         }
     }
 }
